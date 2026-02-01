f9790ef6f906a3364f4492b1decbe3a38dacbf5a
Dariya1317 <dariya.minamova@narxoz.kz>
2026-02-01 13:56:19 +0500

    I did 3th step

--- src/main/java/edu/narxoz/galactic/drones/HeavyDrone.java
+++ src/main/java/edu/narxoz/galactic/drones/HeavyDrone.java
@@ -1,5 +1,10 @@
 package edu.narxoz.galactic.drones;
 
 public class HeavyDrone extends Drone {
+public HeavyDrone(String id, double maxPlayloadKg){
+        super(id, maxPlayloadKg);
+    } 
 
-}
+    @Override
+    public double speedKmPerMin(){}
+}  
